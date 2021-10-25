aa3e2189b7eaf3f1fb24d505d247dd21d372f423
dozken <dosmukhamed@gmail.com>
2021-10-25 23:02:01 +0800

    adding spec for pathVar

--- src/main/java/com/example/demo/model/Status.java
+++ src/main/java/com/example/demo/model/Status.java
@@ -1,12 +1,22 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
+import lombok.*;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "status")
-public class Status {
+public class Status implements Serializable {
+
     @Id
     private Long id;
+
+    private Code code;
 
     public enum Code{
         TO_DO,
